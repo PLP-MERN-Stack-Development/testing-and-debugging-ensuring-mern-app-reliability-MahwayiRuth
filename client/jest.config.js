05fd5ace8035579e7d50a44695599b37c232f6cd
MahwayiRuth <ruthmahwayi@gmail.com>
2025-11-18 19:28:14 +0200

    Create jest.config.js

---
+++ client/jest.config.js
@@ -0,0 +1,26 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.js'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/tests/__mocks__/fileMock.js'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/main.jsx',
+    '!src/tests/**',
+    '!src/cypress/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  testMatch: [
+    '**/src/tests/**/*.test.{js,jsx}'
+  ]
+};
